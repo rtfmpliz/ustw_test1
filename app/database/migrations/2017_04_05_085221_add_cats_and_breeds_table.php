e49512d0ae2e3277c9892d25cfd361bf575b3eec
rtfmpliz <rtfmpliz@gmail.com>
2017-04-05 17:19:03 +0700

    OK CRUD

--- app/database/migrations/2017_04_05_085221_add_cats_and_breeds_table.php
+++ app/database/migrations/2017_04_05_085221_add_cats_and_breeds_table.php
@@ -10,19 +10,22 @@ class AddCatsAndBreedsTable extends Migration {
 	 *
 	 * @return void
 	 */
-	public function up()
-	{
-		//
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		//
-	}
+	public function up(){
+Schema::create('cats', function($table){
+$table->increments('id');
+$table->string('name');
+$table->date('date_of_birth');
+$table->integer('breed_id')->nullable();
+$table->timestamps();
+});
+Schema::create('breeds', function($table){
+$table->increments('id');
+$table->string('name');
+});
+}
+public function down(){
+Schema::drop('cats');
+Schema::drop('breeds');
+}
 
 }
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,6 +10,7 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+		$this->call('BreedsTableSeeder');
 
 		// $this->call('UserTableSeeder');
 	}
